1f82b8b32896dce4799bcc56cec4292ea24b16d1
Pratik <pratik.borde@sapient.com>
2020-09-28 14:12:54 +0100

    feat: treesitter mappings

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,44 +8,63 @@ require "nvim-treesitter.configs".setup {
     enable = true,
     keymaps = {
       -- mappings for incremental selection (visual mappings)
-      init_selection = "gnn", -- maps in normal mode to init the node/scope selection
-      node_incremental = "grn", -- increment to the upper named parent
-      scope_incremental = "grc", -- increment to the upper scope (as defined in locals.scm)
-      node_decremental = "grm" -- decrement to the previous node
+      init_selection = "gss", -- maps in normal mode to init the node/scope selection
+      scope_incremental = "gsu", -- increment to the upper scope (as defined in locals.scm)
+      node_incremental = "gsi", -- increment to the upper named parent
+      node_decremental = "gsd" -- decrement to the previous node
     }
   },
   refactor = {
     highlight_defintions = {
       enable = true
     },
+    highlight_current_scope = {
+      enable = true
+    },
     smart_rename = {
       enable = true,
-      smart_rename = "grr" -- mapping to rename reference under cursor
+      keymaps = {
+        smart_rename = "grr" -- mapping to rename reference under cursor
+      }
     },
     navigation = {
       enable = true,
-      goto_definition = "gnd", -- mapping to go to definition of symbol under cursor
-      list_definitions = "gnD" -- mapping to list all definitions in current file
+      keymaps = {
+        goto_definition = "gnd", -- mapping to go to definition of symbol under cursor
+        list_definitions = "gnD", -- mapping to list all definitions in current file
+        goto_next_usage = "<c-n>",
+        goto_previous_usage = "<c-p>"
+      }
     }
   },
   textobjects = {
+    select = {
+      enable = true,
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      }
+    },
     move = {
       enable = true,
       goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer"
+        ["]f"] = "@function.outer",
+        ["}c"] = "@class.outer"
       },
       goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer"
+        ["]F"] = "@function.outer",
+        ["}C"] = "@class.outer"
       },
       goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer"
+        ["[f"] = "@function.outer",
+        ["{c"] = "@class.outer"
       },
       goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer"
+        ["[F"] = "@function.outer",
+        ["{C"] = "@class.outer"
       }
     }
   }
