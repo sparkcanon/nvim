6860627c90c1f2fb3e9280ee8b98e444bbcbdf32
Pratik <pratik.borde@sapient.com>
2020-10-09 16:07:26 +0100

    chore: replace moonfly with iceberg

--- lua/color.lua
+++ lua/color.lua
@@ -1,3 +1,2 @@
 vim.o.termguicolors = true -- True colors
-vim.cmd([[packadd! vim-moonfly-colors]]) -- Load color scheme package
-vim.cmd("colorscheme moonfly") -- Set colorscheme
+vim.cmd("colorscheme iceberg") -- Set colorscheme
