3a521e7a5348551f953db22f2badc0a053ead306
Pratik <pratik.borde@sapient.com>
2021-05-04 10:38:37 +0100

    chore: refactor plugin init, remove packer_compiled

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,4 @@
 local fn = vim.fn
-local packer = require "packer"
 
 local install_path = fn.stdpath("config") .. "/pack/packer/opt/packer.nvim"
 
@@ -8,6 +7,8 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd "packadd packer.nvim"
 end
 
+vim.cmd "packadd packer.nvim"
+local packer = require "packer"
 packer.init {
   package_root = vim.fn.stdpath("config") .. "/pack"
 }
