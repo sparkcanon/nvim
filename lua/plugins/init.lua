2be78264e4cf720fe84adc62fba0b07e469b38f3
Pratik <pratik.borde@sapient.com>
2021-06-09 16:41:54 +0100

    refactor: move configs to packer

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -27,7 +27,31 @@ packer.startup(
         },
         {
           "hrsh7th/nvim-compe",
-          requires = {{"hrsh7th/vim-vsnip"}, {"rafamadriz/friendly-snippets"}}
+          requires = {{"hrsh7th/vim-vsnip"}, {"rafamadriz/friendly-snippets"}},
+          config = function()
+            require "compe".setup {
+              enabled = true,
+              autocomplete = true,
+              debug = false,
+              min_length = 1,
+              preselect = "enable",
+              throttle_time = 80,
+              source_timeout = 200,
+              incomplete_delay = 400,
+              max_abbr_width = 100,
+              max_kind_width = 100,
+              max_menu_width = 100,
+              documentation = true,
+              source = {
+                path = true,
+                buffer = true,
+                calc = true,
+                nvim_lsp = true,
+                nvim_lua = true,
+                vsnip = true
+              }
+            }
+          end
         }
       }
     }
@@ -49,7 +73,14 @@ packer.startup(
         }
       }
     }
-    use {"lukas-reineke/indent-blankline.nvim", branch = "lua"}
+    use {
+      "lukas-reineke/indent-blankline.nvim",
+      branch = "lua",
+      config = function()
+        vim.g.indent_blankline_char = "│"
+        vim.cmd "let g:indent_blankline_filetype_exclude = ['TelescopePrompt', 'packer']" -- TODO: fix for telescope prompt
+      end
+    }
     use {
       "TimUntersberger/neogit",
       cmd = "Neogit",
@@ -66,21 +97,77 @@ packer.startup(
     }
     use "editorconfig/editorconfig-vim"
     use {"heavenshell/vim-jsdoc", cmd = "JsDoc", run = "make install"}
-    use "hoob3rt/lualine.nvim"
+    use {
+      "hoob3rt/lualine.nvim",
+      config = function()
+        require "lualine".setup {
+          options = {
+            section_separators = "",
+            component_separators = "",
+            theme = "iceberg_dark"
+          },
+          sections = {
+            lualine_a = {"mode"},
+            lualine_b = {"branch", "diff"},
+            lualine_c = {"filename"},
+            lualine_x = {
+              {"diagnostics", sources = {"nvim_lsp"}},
+              "filetype"
+            },
+            lualine_y = {"progress"},
+            lualine_z = {"location"}
+          }
+        }
+      end
+    }
     use "kevinhwang91/nvim-bqf"
     use {"arzg/vim-substrata", opt = true}
-    use "norcalli/nvim-colorizer.lua"
+    use {
+      "norcalli/nvim-colorizer.lua",
+      config = function()
+        require "colorizer".setup {
+          "*", -- Highlight all files, but customize some others.
+          less = {rgb_fn = true}, -- Enable parsing rgb(...) functions in less.
+          sass = {rgb_fn = true}, -- Enable parsing rgb(...) functions in sass.
+          scss = {rgb_fn = true}, -- Enable parsing rgb(...) functions in scss.
+          json = {rgb_fn = true}, -- Enable parsing rgb(...) functions in scss.
+          js = {rgb_fn = true}, -- Enable parsing rgb(...) functions in scss.
+          jsx = {rgb_fn = true}, -- Enable parsing rgb(...) functions in scss.
+          tsx = {rgb_fn = true}, -- Enable parsing rgb(...) functions in scss.
+          ts = {rgb_fn = true} -- Enable parsing rgb(...) functions in scss.
+        }
+      end
+    }
     use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
     use {
       "tpope/vim-commentary",
       "tpope/vim-repeat",
       {"tpope/vim-eunuch", cmd = {"Move", "Rename", "Delete", "Remove"}},
-      "tpope/vim-fugitive",
+      {
+        "tpope/vim-fugitive",
+        config = function()
+          vim.g.fugitive_pty = 0
+        end
+      },
       "tpope/vim-surround"
     }
-    use "justinmk/vim-dirvish"
-    use "voldikss/vim-floaterm"
+    use {
+      "justinmk/vim-dirvish",
+      config = function()
+        vim.g.loaded_netrwPlugin = 1
+        vim.g.dirvish_mode = [[:sort ,^.*[/],]]
+      end
+    }
+    use {
+      "voldikss/vim-floaterm",
+      config = function()
+        vim.g.floaterm_position = "bottom"
+        vim.g.floaterm_width = vim.fn.eval("&columns")
+        vim.g.floaterm_height = 0.3
+      end
+    }
   end
 )
 
-require "plugins/general"
+vim.cmd "packadd! cfilter"
+vim.cmd "packadd! matchit"
