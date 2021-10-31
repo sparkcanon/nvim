60ca1885c9ddc976336ddf45b04b2d08f161baf8
Pratik <prateek_borde@hotmail.com>
2021-10-31 22:56:53 +0000

    feat - jsonls schemas

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -101,11 +101,57 @@ nvim_lsp.tsserver.setup {
 
 -- Json
 nvim_lsp.jsonls.setup {
-  on_attach = function(client)
-    client.resolved_capabilities.document_formatting = false
-    custom_attach(client)
-  end,
-  capabilities = capabilities
+  cmd = {"vscode-json-language-server", "--stdio"},
+  on_attach = custom_attach,
+  capabilities = capabilities,
+  filetypes = {"json", "jsonc"},
+  settings = {
+    json = {
+      -- Schemas https://www.schemastore.org
+      schemas = {
+        {
+          fileMatch = {"package.json"},
+          url = "https://json.schemastore.org/package.json"
+        },
+        {
+          fileMatch = {"tsconfig*.json"},
+          url = "https://json.schemastore.org/tsconfig.json"
+        },
+        {
+          fileMatch = {
+            ".prettierrc",
+            ".prettierrc.json",
+            "prettier.config.json"
+          },
+          url = "https://json.schemastore.org/prettierrc.json"
+        },
+        {
+          fileMatch = {".eslintrc", ".eslintrc.json"},
+          url = "https://json.schemastore.org/eslintrc.json"
+        },
+        {
+          fileMatch = {".babelrc", ".babelrc.json", "babel.config.json"},
+          url = "https://json.schemastore.org/babelrc.json"
+        },
+        {
+          fileMatch = {"lerna.json"},
+          url = "https://json.schemastore.org/lerna.json"
+        },
+        {
+          fileMatch = {"now.json", "vercel.json"},
+          url = "https://json.schemastore.org/now.json"
+        },
+        {
+          fileMatch = {
+            ".stylelintrc",
+            ".stylelintrc.json",
+            "stylelint.config.json"
+          },
+          url = "http://json.schemastore.org/stylelintrc.json"
+        }
+      }
+    }
+  }
 }
 
 -- Lua
