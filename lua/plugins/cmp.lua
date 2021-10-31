38e116bfb765cfff7e1f590d3cee6e7aebfd67ae
Pratik <prateek_borde@hotmail.com>
2021-10-31 23:03:16 +0000

    feat - cmp vsnip tab completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,4 +1,12 @@
 local cmp = require "cmp"
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
 
 cmp.setup(
   {
@@ -21,7 +29,31 @@ cmp.setup(
           c = cmp.mapping.close()
         }
       ),
-      ["<CR>"] = cmp.mapping.confirm({select = true})
+      ["<CR>"] = cmp.mapping.confirm({select = true}),
+      ["<Tab>"] = cmp.mapping(
+        function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif vim.fn["vsnip#available"](1) == 1 then
+            feedkey("<Plug>(vsnip-expand-or-jump)", "")
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+          end
+        end,
+        {"i", "s"}
+      ),
+      ["<S-Tab>"] = cmp.mapping(
+        function()
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+            feedkey("<Plug>(vsnip-jump-prev)", "")
+          end
+        end,
+        {"i", "s"}
+      )
     },
     sources = cmp.config.sources(
       {
