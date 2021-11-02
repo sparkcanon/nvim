7fb10d7570fcd1ddef8e072c009451b16dcc7581
Pratik <prateek_borde@hotmail.com>
2021-11-02 20:36:36 +0000

    chore - remove unused colors

--- lua/utils/color.lua
+++ lua/utils/color.lua
@@ -5,9 +5,7 @@ local C = {}
 -- Modify buffer colors
 function C.ModifyBufferColors()
   cmd [[highlight! EndOfBuffer ctermbg=NONE ctermfg=241 guibg=NONE]]
-  cmd [[highlight! VertSplit cterm=NONE ctermbg=NONE gui=NONE guibg=NONE]]
-  cmd [[highlight! link HighlightedyankRegion Visual]]
-  cmd [[highlight! LineNr ctermbg=NONE guibg=NONE]]
+  cmd [[highlight! SignColumn ctermbg=NONE guibg=NONE]]
 
   -- Hint: The below makes buffer transparent
   cmd [[highlight! Normal guibg=NONE]]
