723b089dc021df4eb9168b84865a19a43a637127
Pratik Borde <p.borde@iwoca.co.uk>
2022-09-16 16:28:46 +0100

    chore: ivy looks ugly with current theme

--- lua/spark/init.lua
+++ lua/spark/init.lua
@@ -255,7 +255,6 @@ require('telescope').setup {
   },
   extensions = {
     file_browser = {
-      theme = "ivy",
       -- disables netrw and use telescope-file-browser in its place
       hijack_netrw = true,
       mappings = {
