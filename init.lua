8ca9e034b8284b8489ab7b8b9c4cb81ee203c99e
Pratik Borde <pratik@Pratiks-MacBook-Pro.local>
2022-09-19 13:38:32 +0100

    feat: sort file browser, mapping

--- init.lua
+++ init.lua
@@ -327,7 +327,7 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sb', ':Telescope file_browser path=%:p:h<CR>', { desc = '[S]earch file [B]rowser' })
+vim.keymap.set('n', '<leader>sF', ':Telescope file_browser grouped=true path=%:p:h<CR>', { desc = '[S]earch for [F]iles' })
 vim.keymap.set('n', '<leader>sm', ':Telescope harpoon marks<CR>', { desc = '[S]earch [M]arks' })
 
 -- [[ Configure Treesitter ]]
