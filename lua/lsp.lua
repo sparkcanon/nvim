0ad4fdc4969a88dcef6bcfb80ee39a0ed4d02c55
sparkcanon <prateek_borde@hotmail.com>
2022-10-25 23:33:14 +0100

    fix: deparated property

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -56,7 +56,7 @@ local on_attach = function(_, bufnr)
 end
 
 -- nvim-cmp supports additional completion capabilities
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 -- [[ Configure LSP servers ]]
 -- Enable the following language servers
