187951c7ed722f1af8c72cb582d73b16bba954d7
sparkcanon <prateek_borde@hotmail.com>
2022-12-12 22:19:16 +0000

    feat: add indent lines

--- lua/init.lua
+++ lua/init.lua
@@ -28,6 +28,16 @@ require('packer').startup(function(use)
     end
   }
   use {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("indent_blankline").setup {
+        -- for example, context is off by default, use this to turn it on
+        show_current_context = true,
+        show_current_context_start = true,
+      }
+    end
+  }
+  use {
     'lewis6991/gitsigns.nvim', -- Add git related info in the signs columns and popups
     requires = { 'nvim-lua/plenary.nvim' },
     config = function()
